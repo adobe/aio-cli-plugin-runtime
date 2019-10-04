75846869bfc95a45e8dbb062460c61c88ee93c98
Jesse MacFadyen <purplecabbage@gmail.com>
2019-10-04 16:41:38 -0700

    added --last/-l flags to view last log, also shortcut `aio rt log -l` (#44)
    * added --last/-l flags to view last log, also shortcut `aio rt log -l`
    
    * full test coverage, updated docs

--- test/commands/runtime/activation/logs.test.js
+++ test/commands/runtime/activation/logs.test.js
@@ -31,11 +31,14 @@ test('aliases', async () => {
   expect(TheCommand.aliases.length).toBeGreaterThan(0)
 })
 
-test('args', async () => {
+test('args and flags', async () => {
   const logName = TheCommand.args[0]
   expect(logName.name).toBeDefined()
   expect(logName.name).toEqual('activationID')
-  expect(logName.required).toEqual(true)
+  expect(logName.required).toEqual(false)
+
+  expect(TheCommand.flags).toBeDefined()
+  expect(TheCommand.flags.last).toBeDefined()
 })
 
 describe('instance methods', () => {
@@ -68,6 +71,41 @@ describe('instance methods', () => {
         .then(() => done.fail('does not throw error'))
         .catch(() => {
           expect(handleError).toHaveBeenLastCalledWith('failed to retrieve the logs', new Error('an error'))
+          done()
+        })
+    })
+
+    test('retrieve last log -l', () => {
+      const listCmd = ow.mockResolved('activations.list', [{ activationId: '12345' }])
+      const logCmd = ow.mockResolved(owAction, { logs: ['line1', 'line2', 'line3'] })
+      command.argv = ['-l']
+      return command.run()
+        .then(() => {
+          expect(listCmd).toHaveBeenCalledWith(expect.objectContaining({ limit: 1 }))
+          expect(logCmd).toHaveBeenCalledWith('12345')
+          expect(stdout.output).toMatch('line3')
+        })
+    })
+
+    test('retrieve last log --last', () => {
+      const listCmd = ow.mockResolved('activations.list', [{ activationId: '12345' }])
+      const logCmd = ow.mockResolved(owAction, { logs: ['line1', 'line2', 'line3'] })
+      command.argv = ['--last']
+      return command.run()
+        .then(() => {
+          expect(listCmd).toHaveBeenCalledWith(expect.objectContaining({ limit: 1 }))
+          expect(logCmd).toHaveBeenCalledWith('12345')
+          // expect(stdout.output).toMatch('line3')
+        })
+    })
+
+    test('errors if np axId or --last flag', (done) => {
+      command.argv = []
+      const error = jest.spyOn(command, 'error')
+      return command.run()
+        .then(() => done.fail('does not throw error'))
+        .catch(() => {
+          expect(error).toHaveBeenLastCalledWith('Missing required arg: `activationID`')
           done()
         })
     })
