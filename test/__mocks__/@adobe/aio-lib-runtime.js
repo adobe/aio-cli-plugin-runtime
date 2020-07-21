2d4dbee84b982045f490a1031ac294e26a35f2b2
Jesse MacFadyen <purplecabbage@gmail.com>
2020-07-21 15:10:41 -0700

    add mock for runtime-lib

---
+++ test/__mocks__/@adobe/aio-lib-runtime.js
@@ -0,0 +1,27 @@
+
+module.exports = {
+  utils: {
+    createKeyValueObjectFromArray: jest.fn(),
+    createKeyValueArrayFromObject: jest.fn(),
+    createKeyValueArrayFromFile: jest.fn(),
+    createKeyValueArrayFromFlag: jest.fn(),
+    createKeyValueObjectFromFlag: jest.fn(),
+    createKeyValueObjectFromFile: jest.fn(),
+    parsePathPattern: jest.fn(),
+    parsePackageName: jest.fn(),
+    createComponentsfromSequence: jest.fn(),
+    processInputs: jest.fn(),
+    returnUnion: jest.fn(),
+    deployPackage: jest.fn(),
+    undeployPackage: jest.fn(),
+    processPackage: jest.fn(),
+    setPaths: jest.fn(),
+    getProjectEntities: jest.fn(),
+    syncProject: jest.fn(),
+    findProjectHashonServer: jest.fn(),
+    getProjectHash: jest.fn(),
+    addManagedProjectAnnotations: jest.fn(),
+    printLogs: jest.fn()
+  },
+  init: jest.fn()
+}
