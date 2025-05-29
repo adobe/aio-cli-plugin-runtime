345d82602a69d866dc71c7aaf85efd73efd8b386
Shazron Abdullah <shazron@gmail.com>
2025-05-29 12:48:59 +0800

    fix: error when current ims context is undefined

--- src/DeployServiceCommand.js
+++ src/DeployServiceCommand.js
@@ -38,7 +38,7 @@ class DeployServiceCommand extends RuntimeBaseCommand {
     let contextName = CLI // default
     const currentContext = await context.getCurrent() // potential override
 
-    if (currentContext !== CLI) {
+    if (currentContext && currentContext !== CLI) {
       contextName = currentContext
     } else {
       await context.setCli({ 'cli.bare-output': true }, false) // set this globally
